aaa478f540e97c641f7d0f0845e0191f2805b75b
proshanto7 <proshantosarker153@gmail.com>
2025-12-01 12:29:04 +0600

    destinations part change

--- src/components/destination/Destination.tsx
+++ src/components/destination/Destination.tsx
@@ -1,10 +1,29 @@
 
+import Container from '../common/Container'
 import Title from '../title/Title'
 
 function Destination() {
   return (
     <section className='pt-[400px]'>
+
+<Container>
+
+
       <Title subtitle='Top Destinations' title='Top Selling'/>
+
+    <div>
+
+
+
+
+
+    </div>
+
+
+
+</Container>
+
+
     </section>
   )
 }
